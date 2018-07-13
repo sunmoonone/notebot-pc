6dfbc59b0461b01f29e1880dde4f452670a5bba8
sunmoonone <sunmoonone@foxmail.com>
2018-07-13 18:45:23 +0800

    implemented class Tree

--- src/renderer/Tree.js
+++ src/renderer/Tree.js
@@ -1,108 +1,275 @@
-//todo: rewrite using es6
-function Node (data) {
-  this.data = data
-  this.parent = null
-  this.children = []
-}
+// todo: rewrite using es6
+export class TreeNode {
+  constructor (data) {
+    this._data = data
+    this._parent = null
+    this._children = []
+  }
 
-function Tree (data) {
-  var node = new Node(data)
-  this._root = node
-}
+  get data () {
+    return this._data
+  }
 
-Tree.prototype.traverseDF = function (callback) {
-  // this is a recurse and immediately-invoking function
-  ;(function recurse (currentNode) {
-    // step 2
-    for (var i = 0, length = currentNode.children.length; i < length; i++) {
-      // step 3
-      recurse(currentNode.children[i])
-    }
+  get children () {
+    return this._children
+  }
 
-    // step 4
-    callback(currentNode)
-
-    // step 1
-  })(this._root)
-}
-
-Tree.prototype.traverseBF = function (callback) {
-  var queue = new Queue()
-
-  queue.enqueue(this._root)
-
-  currentTree = queue.dequeue()
-
-  while (currentTree) {
-    for (var i = 0, length = currentTree.children.length; i < length; i++) {
-      queue.enqueue(currentTree.children[i])
-    }
-
-    callback(currentTree)
-    currentTree = queue.dequeue()
+  get siblings () {
+    var thiss = this
+    return !this._parent
+      ? []
+      : this._parent._children.filter(function (_child) {
+        return _child !== thiss
+      })
   }
 }
 
-Tree.prototype.contains = function (callback, traversal) {
-  traversal.call(this, callback)
-}
+export class Tree {
+  constructor (data) {
+    this._root = new TreeNode(data)
+    this._searchStrategy = 'df'
+    this._current = this._root
+  }
 
-Tree.prototype.add = function (data, toData, traversal) {
-  var child = new Node(data),
-    parent = null,
-    callback = function (node) {
-      if (node.data === toData) {
-        parent = node
+  static DF = 'df'
+  static BF = 'bf'
+
+  get currentNode () {
+    return this._current
+  }
+
+  get rootNode () {
+    return this._root
+  }
+
+  get isEmpty () {
+    return this._root === null
+  }
+
+  set searchStrategy (strategy) {
+    this._searchStrategy = strategy
+  }
+
+  traverseDF (matcher) {
+    // Hold the node when found
+    var foundNode = null
+
+    // Find node recursively
+    ;(function recur (node) {
+      if (matcher(node)) {
+        foundNode = node
+        return foundNode
+      } else {
+        node._children.some(recur)
       }
-    }
+    })(this._root)
 
-  this.contains(callback, traversal)
-
-  if (parent) {
-    parent.children.push(child)
-    child.parent = parent
-  } else {
-    throw new Error('Cannot add node to a non-existent parent.')
-  }
-}
-
-Tree.prototype.remove = function (data, fromData, traversal) {
-  var tree = this,
-    parent = null,
-    childToRemove = null,
-    index
-
-  var callback = function (node) {
-    if (node.data === fromData) {
-      parent = node
-    }
+    return foundNode
   }
 
-  this.contains(callback, traversal)
+  traverseBF (matcher) {
+    var foundNode = null
+    // Find nodes recursively
+    ;(function expand (queue) {
+      while (queue.length) {
+        var current = queue.splice(0, 1)[0]
+        if (matcher(current)) {
+          foundNode = current
+          return
+        }
+        current._children.forEach(function (_child) {
+          queue.push(_child)
+        })
+      }
+    })([this._root])
 
-  if (parent) {
-    index = findIndex(parent.children, data)
+    return foundNode
+  }
 
-    if (index === undefined) {
-      throw new Error('Node to remove does not exist.')
+  first (matcher) {
+    if (this._searchStrategy === Tree.DF) {
+      return this.traverseDF(matcher)
     } else {
-      childToRemove = parent.children.splice(index, 1)
-    }
-  } else {
-    throw new Error('Parent does not exist.')
-  }
-
-  return childToRemove
-}
-
-function findIndex (arr, data) {
-  var index
-
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i].data === data) {
-      index = i
+      return this.traverseDF(matcher)
     }
   }
+  contains (matcher) {
+    return this.first(matcher) !== null
+  }
 
-  return index
+  /**
+   * @param {function} cmpFn  required Specifies a function that defines the sort order.
+   * a < b returns a value less than 0
+   * a > b returns a value greater than 0
+   * a == b returns 0
+   * the smaller elements get lower index in the sorted array
+   */
+  sort (cmpFn) {}
+
+  pruneAll () {
+    if (this._root) this.trimBranchFrom(this._root)
+    return this
+  }
+
+  /**
+   * Remove an entire branch starting with specified node.
+   * @param {TreeNode} node
+   */
+  trimBranchFrom (node) {
+    var thiss = this
+
+    // trim brach recursively
+    ;(function recur (node) {
+      node._children.forEach(recur)
+      node._children = []
+      node._data = null
+    })(node)
+
+    // Update Current Node
+    if (node._parent) {
+      node._parent._children.splice(node._parent._children.indexOf(node), 1)
+      thiss._current = node._parent
+    } else {
+      thiss._root = thiss._current = null
+    }
+  }
+
+  /**
+   * Creates a {@link TreeNode} that contains the data provided and insert it in a tree.
+   * New node gets inserted to the `_currentNode` which updates itself upon every insertion and deletion.
+   *
+   * @method insert
+   * @memberof Tree
+   * @instance
+   * @param {object} data - data that has to be stored in tree-node.
+   * @return {object} - instance of {@link TreeNode} that represents node inserted.
+   * @example
+   *
+   * // Insert single value
+   * tree.insert(183);
+   *
+   * // Insert array of values
+   * tree.insert([34, 565, 78]);
+   *
+   * // Insert complex data
+   * tree.insert({
+   *   key: '#berries',
+   *   value: { name: 'Apple', color: 'Red'}
+   * });
+   */
+  insert (data) {
+    var node = new TreeNode(data)
+    if (this._root === null && this._current === null) {
+      this._root = this._current = node
+    } else {
+      node._parent = this._current
+      this._current._children.push(node)
+      this._current = node
+    }
+    return node
+  }
+
+  /**
+   * Inserts node to a particular node present in the tree. Particular node here is searched
+   * in the tree based on the criteria provided.
+   *
+   * @method insertTo
+   * @memberof Tree
+   * @instance
+   * @param {function} matcher - Callback function that specifies the search criteria
+   * for node to which new node is to be inserted. Criteria callback here receives {@link TreeNode#_data}
+   * in parameter and MUST return boolean indicating whether that data satisfies your criteria.
+   * @param {object} data - that has to be stored in tree-node.
+   * @return {object} - instance of {@link TreeNode} that represents node inserted.
+   * @example
+   *
+   * // Insert data
+   * tree.insert({
+   *   key: '#apple',
+   *   value: { name: 'Apple', color: 'Red'}
+   * });
+   *
+   * // New Data
+   * var greenApple = {
+   *  key: '#greenapple',
+   *  value: { name: 'Green Apple', color: 'Green' }
+   * };
+   *
+   * // Insert data to node which has `key` = #apple
+   * tree.insertTo(function(data){
+   *  return data.key === '#apple'
+   * }, greenApple);
+   */
+  insertTo (matcher, data) {
+    var node = this.first(matcher)
+    if (node === null) return null
+    return this.insertToNode(node, data)
+  }
+
+  /**
+   * Inserts node to a particular node present in the tree. Particular node here is an instance of {@link TreeNode}
+   *
+   * @method insertToNode
+   * @memberof Tree
+   * @instance
+   * @param {function} node -  {@link TreeNode} to which data node is to be inserted.
+   * @param {object} data - that has to be stored in tree-node.
+   * @return {object} - instance of {@link TreeNode} that represents node inserted.
+   * @example
+   *
+   * // Insert data
+   * var node = tree.insert({
+   *   key: '#apple',
+   *   value: { name: 'Apple', color: 'Red'}
+   * });
+   *
+   * // New Data
+   * var greenApple = {
+   *  key: '#greenapple',
+   *  value: { name: 'Green Apple', color: 'Green' }
+   * };
+   *
+   * // Insert data to node
+   * tree.insertToNode(node, greenApple);
+   */
+  insertToNode (node, data) {
+    var newNode = new TreeNode(data)
+    newNode._parent = node
+    node._children.push(newNode)
+    this._current = newNode
+    return newNode
+  }
+
+  /**
+   * Removes a node from tree and updates `_currentNode` to parent node of node removed.
+   *
+   * @method remove
+   * @memberof Tree
+   * @instance
+   * @param {object} node - {@link TreeNode} that has to be removed.
+   * @param {boolean} trim - indicates whether to remove entire branch from the specified node.
+   */
+  remove (node, trim) {
+    if (trim || node === this._root) {
+      // Trim Entire branch
+      this.trimBranchFrom(node)
+    } else {
+      // Upate children's parent to grandparent
+      node._children.forEach(function (_child) {
+        _child._parent = node._parent
+        node._parent._child.push(_child)
+      })
+
+      // Delete itslef from parent child array
+      node._parent._children.splice(node._parent._children.indexOf(node), 1)
+
+      // Update Current Node
+      this._current = node._parent
+
+      // Clear Child Array
+      node._child = []
+      node._parent = null
+      node._data = null
+    }
+  }
 }
